708a40e4bd737a92cbf6d41261776ce85343f992
suubinkim <0vbbbv0@gmail.com>
2022-02-01 21:10:54 +0900

    fix : 테스트 주석처리

--- src/test/java/site/cradle/cradle_back/CradleTocradleBackApplicationTests.java
+++ src/test/java/site/cradle/cradle_back/CradleTocradleBackApplicationTests.java
@@ -3,11 +3,11 @@ package site.cradle.cradle_back;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class CradleTocradleBackApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class CradleTocradleBackApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
