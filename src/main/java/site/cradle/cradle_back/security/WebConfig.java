1ce57c8c4122c9475a3bd860fa09e6cb55f9c02c
suubinkim <0vbbbv0@gmail.com>
2022-02-03 17:06:30 +0900

    allowedOrigins 수정

--- src/main/java/site/cradle/cradle_back/security/WebConfig.java
+++ src/main/java/site/cradle/cradle_back/security/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:63342", "https://www.subinee.shop", "http://localhost:3000", "https://api.subinee.shop")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 }
