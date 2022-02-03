967314c160a6e2a9e98c14906f547c809f08a434
suubinkim <0vbbbv0@gmail.com>
2022-02-03 14:38:07 +0900

    WebSecurityConfig 추가

--- src/main/java/site/cradle/cradle_back/security/WebSecurityConfig.java
+++ src/main/java/site/cradle/cradle_back/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package site.cradle.cradle_back.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -30,6 +31,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.headers().frameOptions().disable();
 
         http.authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**/*").permitAll()
+                .antMatchers(HttpMethod.GET, "/").permitAll()
                 .antMatchers("/h2-console/**").permitAll()
                 .antMatchers("/login").permitAll()
                 .antMatchers("/signup").permitAll()
