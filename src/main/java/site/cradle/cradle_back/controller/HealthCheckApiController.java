41434e8599851cf57007588d5e615daf640570c8
suubinkim <0vbbbv0@gmail.com>
2022-04-21 16:50:59 +0900

    ci/cd : eb 변경

--- src/main/java/site/cradle/cradle_back/controller/HealthCheckApiController.java
+++ src/main/java/site/cradle/cradle_back/controller/HealthCheckApiController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckApiController {
 
-    @GetMapping("/healthCheck")
+    @GetMapping("/")
     public String health() {
         return "ok";
     }
